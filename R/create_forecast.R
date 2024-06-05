a27f4019f6b82b150ce79cf916be56eb9e892b4b
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-05 12:28:40 -0400

    rename parameter data -> dat to avoid shadowing builtin

--- R/create_forecast.R
+++ R/create_forecast.R
@@ -3,7 +3,7 @@
 #' `create_forecast()` creates a forecast object given data and optional metadata.
 #' It accepts a variety of forecast formats as input and intelligently converts them into a standardized format.
 #'
-#' @param data Forecast data. Either a data frame or list of data frames.
+#' @param dat Forecast data. Either a data frame or list of data frames.
 #'  An ensemble of forecasts can be provided as a list of data frames, in which case it will be aggregated into a single data frame.
 #'  The data frame(s) should contain a column named `time`, which may be of integers, dates, or date-times.
 #'  No mixing of types is allowed (e.g. `time` may not contain both integers and date-times).
@@ -18,21 +18,21 @@
 #'
 #' @examples
 #' # TBD
-create_forecast <- function(data, name=NULL, forecast_time=NULL) {
+create_forecast <- function(dat, name=NULL, forecast_time=NULL) {
     forecast <- list(name=name, forecast_time=forecast_time)
     # we check for data frame first since data frames are also lists
 
     # A single data frame 
-    if(is.data.frame(data)) {
+    if(is.data.frame(dat)) {
         # validate data frame & get its format
-        format <- get_format(data)
+        format <- get_format(dat)
         # store the format as metadata
         forecast$time_type <- format$time_type
         forecast$data_type <- format$data_type
     }
     
     # A list of data frames, to be combined.
-    else if (is.list(data)) {
+    else if (is.list(dat)) {
         # TODO: check that all data frames are valid and compatible
 
         # TODO: combine the data frames using another (exported) function
