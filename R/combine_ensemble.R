b5d0e0cb60efce64e2a2bd71fd42170b7e6e7bb6
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-05 14:10:16 -0400

    introduce helper function

--- R/combine_ensemble.R
+++ R/combine_ensemble.R
@@ -17,11 +17,12 @@ combine_ensemble <- function(...) {
     # call combine_data_frames()
 }
 
-#' Combine a group of data frames into one
+#' Combine a group of forecast data frames into one
 #'
 #' `combine_data_frames()` converts a list of forecast data frames
 #' into a single data frame containing all the aggregated data points in one column.
-#'
+#' It performs no input validation so it is not user-facing.
+#' 
 #' @param dfs A list of forecast data frames, with raw data points and the same time types.
 #'
 #' @returns A data frame containing the aggregated data.
@@ -29,5 +30,23 @@ combine_ensemble <- function(...) {
 #' @examples
 #' # TBD
 combine_data_frames <- function(dfs) {
+    if(length(dfs) == 0) {
+        stop("dfs has length 0")
+    }
+
+    # accumulate
+    out <- dfs[[1]]
+}
+
+#' Combine two forecast data frames into one
+#'
+#' Helper for `combine_data_frames()`.
+#' See `?combine_data_frames()` for details.
+#'
+#' @param df1 The first data frame.
+#' @param df2 The second data frame.
+#'
+#' @returns The combined data frame.
+combine_two_data_frames <- function(df1, df2) {
 
 }
