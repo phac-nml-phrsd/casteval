99e4b3e2334130471d54699f5d0069692f613c16
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-06 15:23:38 -0400

    remove casteval::: prefix in tests

--- tests/testthat/test-combine_data_frames.R
+++ tests/testthat/test-combine_data_frames.R
@@ -1,33 +1,33 @@
 ## widen_NULL() tests
 
 test_that("widen_NULL() accepts empty list", {
-  expect_equal(casteval:::widen_NULL(list(), 1), list())
+  expect_equal(widen_NULL(list(), 1), list())
 })
 
 test_that("widen_NULL() widens to 1", {
   expect_equal(
-    casteval:::widen_NULL(list(NULL, 1, NULL, 2, NULL, 3), 1),
+    widen_NULL(list(NULL, 1, NULL, 2, NULL, 3), 1),
     list(NA_real_, 1, NA_real_, 2, NA_real_, 3)
   )
 })
 
 test_that("widen_NULL() widens to multiple", {
   expect_equal(
-    casteval:::widen_NULL(list(NULL, c(1,2,3), NULL), 3),
+    widen_NULL(list(NULL, c(1,2,3), NULL), 3),
     list(c(NA_real_, NA_real_, NA_real_), c(1,2,3), c(NA_real_, NA_real_, NA_real_))
   )
 })
 
 test_that("widen_NULL() widens to 0", {
   expect_equal(
-    casteval:::widen_NULL(list(NULL, 1, c(2,3,4), NULL), 0),
+    widen_NULL(list(NULL, 1, c(2,3,4), NULL), 0),
     list(numeric(0), 1, c(2,3,4), numeric(0))
   )
 })
 
 test_that("widen_NULL() does not modify non-NULL", {
   expect_equal(
-    casteval:::widen_NULL(list(list(), NA, logical(0), numeric(0), NA_real_, 1, c(1,2,3), "hi", 0, FALSE), 2),
+    widen_NULL(list(list(), NA, logical(0), numeric(0), NA_real_, 1, c(1,2,3), "hi", 0, FALSE), 2),
     list(list(), NA, logical(0), numeric(0), NA_real_, 1, c(1,2,3), "hi", 0, FALSE)
   )
 })
@@ -36,11 +36,11 @@ test_that("widen_NULL() does not modify non-NULL", {
 
 test_that("combine_two_data_frames() demands non-empty data frames", {
   expect_error(
-    casteval:::combine_two_data_frames(data.frame(), data.frame(a=1)),
+    combine_two_data_frames(data.frame(), data.frame(a=1)),
     "nrow.*df1"
   )
   expect_error(
-    casteval:::combine_two_data_frames(data.frame(a=1), data.frame()),
+    combine_two_data_frames(data.frame(a=1), data.frame()),
     "nrow.*df2"
   )
 })
@@ -48,7 +48,7 @@ test_that("combine_two_data_frames() demands non-empty data frames", {
 test_that("combine_two_data_frames() works with all time types", {
   # numeric
   expect_equal(
-    casteval:::combine_two_data_frames(
+    combine_two_data_frames(
       data.frame(time=c(1,2,3), raw=c(10, 11, 12)),
       data.frame(time=c(3,2,1), raw=c(20, 21, 22))
     ),
@@ -63,7 +63,7 @@ test_that("combine_two_data_frames() works with all time types", {
 
   # dates
   expect_equal(
-    casteval:::combine_two_data_frames(
+    combine_two_data_frames(
       data.frame(time=c(lubridate::ymd("2024-01-01"),lubridate::ymd("2024-01-02"),lubridate::ymd("2024-01-03")), raw=c(10, 11, 12)),
       data.frame(time=c(lubridate::ymd("2024-01-03"),lubridate::ymd("2024-01-02"),lubridate::ymd("2024-01-01")), raw=c(20, 21, 22))
     ),
@@ -76,7 +76,7 @@ test_that("combine_two_data_frames() works with all time types", {
 
   # date-times
   expect_equal(
-    casteval:::combine_two_data_frames(
+    combine_two_data_frames(
       data.frame(time=c(lubridate::ymd_hms("2024-01-01_01-01-01"),lubridate::ymd_hms("2024-01-02_01-01-01"),lubridate::ymd_hms("2024-01-03_01-01-01")), raw=c(10, 11, 12)),
       data.frame(time=c(lubridate::ymd_hms("2024-01-03_01-01-01"),lubridate::ymd_hms("2024-01-02_01-01-01"),lubridate::ymd_hms("2024-01-01_01-01-01")), raw=c(20, 21, 22))
     ),
@@ -90,7 +90,7 @@ test_that("combine_two_data_frames() works with all time types", {
 
 test_that("combine_two_data_frames() handles NAs correctly", {
   expect_equal(
-    casteval:::combine_two_data_frames(
+    combine_two_data_frames(
       data.frame(time=c(1, 2, 3), raw=c(10, 11, 12)),
       data.frame(time=c(2, 3, 4), raw=c(20, 21, 22))
     ),
@@ -105,7 +105,7 @@ test_that("combine_two_data_frames() handles NAs correctly", {
 test_that("combine_two_data_frames() works with list columns", {
   # df1 list column
   expect_equal(
-    casteval:::combine_two_data_frames(
+    combine_two_data_frames(
       # we use tibbles here for convenience
       dplyr::tibble(time=c(1,2,3), raw=list(c(10,11), c(12,13), c(14,15))),
       dplyr::tibble(time=c(2,3,4), raw=c(20,21,22))
@@ -115,7 +115,7 @@ test_that("combine_two_data_frames() works with list columns", {
 
   # df2 list column
   expect_equal(
-    casteval:::combine_two_data_frames(
+    combine_two_data_frames(
       dplyr::tibble(time=c(2,3,4), raw=c(20,21,22)),
       dplyr::tibble(time=c(1,2,3), raw=list(c(10,11), c(12,13), c(14,15)))
     ),
@@ -124,7 +124,7 @@ test_that("combine_two_data_frames() works with list columns", {
 
   # both list column
   expect_equal(
-    casteval:::combine_two_data_frames(
+    combine_two_data_frames(
       dplyr::tibble(time=c(1,2,3), raw=list(10, 11, 12)),
       dplyr::tibble(time=c(2,3,4), raw=list(20, 21, 22))
     ),
@@ -135,21 +135,21 @@ test_that("combine_two_data_frames() works with list columns", {
 # this is unlikely to occur but for robustness it should still work
 test_that("combine_two_data_frames() handles empty values", {
   expect_equal(
-    casteval:::combine_two_data_frames(
+    combine_two_data_frames(
       dplyr::tibble(time=c(1,2), raw=c(1,2)),
       dplyr::tibble(time=c(1,2), raw=list(NULL, numeric(0)))
     ),
     dplyr::tibble(time=c(1,2), raw=list(1,2))
   )
   expect_equal(
-    casteval:::combine_two_data_frames(
+    combine_two_data_frames(
       dplyr::tibble(time=c(1,2), raw=list(NULL, numeric(0))),
       dplyr::tibble(time=c(1,2), raw=c(1,2))
     ),
     dplyr::tibble(time=c(1,2), raw=list(1,2))
   )
   expect_equal(
-    casteval:::combine_two_data_frames(
+    combine_two_data_frames(
       dplyr::tibble(time=c(1,2), raw=list(NULL, numeric(0))),
       dplyr::tibble(time=c(1,2), raw=list(numeric(0),NULL))
     ),
@@ -165,14 +165,14 @@ test_that("combine_data_frames() demands > 0 data frames", {
 
 test_that("combine_data_frames() accepts 1 data frame", {
   expect_equal(
-    casteval:::combine_data_frames(list(data.frame(time=c(1,2,3), raw=c(4,5,6)))),
+    combine_data_frames(list(data.frame(time=c(1,2,3), raw=c(4,5,6)))),
     data.frame(time=c(1,2,3), raw=c(4,5,6))
   )
 })
 
 test_that("combine_data_frames() works", {
   expect_equal(
-    casteval:::combine_data_frames(list(
+    combine_data_frames(list(
       data.frame(time=c(1,2,3), raw=c(10,11,12)),
       data.frame(time=c(2,3,4), raw=c(20,21,22)),
       data.frame(time=c(3,4,5), raw=c(30,31,32))
@@ -187,7 +187,7 @@ test_that("combine_data_frames() works", {
 
 test_that("combine_data_frames() discards extraneous columns", {
   expect_equal(
-    casteval:::combine_data_frames(list(
+    combine_data_frames(list(
       data.frame(time=c(1,2,3), raw=c(10,11,12), whatever=c(100,200,300)),
       data.frame(time=c(2,3,4), raw=c(20,21,22), whatever=c(-1,-2,-3)),
       data.frame(time=c(3,4,5), raw=c(30,31,32), something=c("a","b","c"))
--- tests/testthat/test-get_format.R
+++ tests/testthat/test-get_format.R
@@ -1,42 +1,49 @@
 test_that("column_all() works", {
   expect_equal(
-    casteval:::column_all(list(1, 2.5, 1.4e7, -10, 5L, Inf, NaN), is.numeric),
+    column_all(list(1, 2.5, 1.4e7, -10, 5L, Inf, NaN), is.numeric),
     TRUE
   )
   expect_equal(
-    casteval:::column_all(list(1, 2, 3), function(x) {x < 3}),
+    column_all(list(1, 2, 3), function(x) {x < 3}),
     FALSE
   )
   expect_equal(
-    casteval:::column_all(list(1, NA), is.numeric),
+    column_all(list(1, NA), is.numeric),
     FALSE
   )
   expect_equal(
-    casteval:::column_all(c(1,NA), is.numeric),
+    column_all(c(1,NA), is.numeric),
     TRUE
   )
 })
 
 test_that("get_time_type() works", {
-  expect_error(casteval:::get_time_type(list()), "no times present")
+  expect_error(get_time_type(list()), "no times present")
   expect_equal(
-    casteval:::get_time_type(c(1,2,3)),
+    get_time_type(c(1,2,3)),
     "numeric"
   )
   expect_equal(
-    casteval:::get_time_type(c(lubridate::ymd("2024-01-01"), lubridate::ymd("2024-01-02"))),
+    get_time_type(c(lubridate::ymd("2024-01-01"), lubridate::ymd("2024-01-02"))),
     "date"
   )
   expect_equal(
-    casteval:::get_time_type(list(lubridate::ymd_hms("2024-01-01_12:34:56"))),
+    get_time_type(list(lubridate::ymd_hms("2024-01-01_12:34:56"))),
     "date-time"
   )
   expect_error(
-    casteval:::get_time_type(list(1, lubridate::ymd("2024-01-01"))),
+    get_time_type(list(1, lubridate::ymd("2024-01-01"))),
     "inconsistent"
   )
   expect_error(
-    casteval:::get_time_type(list("January 1", "January 2")),
+    get_time_type(list("January 1", "January 2")),
     "unsupported"
   )
 })
+
+# test_that("get_format() works", {
+#   expect_equal(
+#     column_all(c(1,2), is.numeric),
+#     TRUE
+#   )
+# })
