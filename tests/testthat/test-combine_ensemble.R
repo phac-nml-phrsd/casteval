327c8b91e972e9e3b6de063c5a8a195cdc0e3311
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-06 11:06:03 -0400

    more combine_two_data_frames() tests

--- tests/testthat/test-combine_ensemble.R
+++ tests/testthat/test-combine_ensemble.R
@@ -39,7 +39,8 @@ test_that("combine_two_data_frames() demands non-empty data frames", {
   )
 })
 
-test_that("combine_two_data_frames() works", {
+test_that("combine_two_data_frames() works with all time types", {
+  # numeric
   expect_equal(
     casteval:::combine_two_data_frames(
       data.frame(time=c(1,2,3), raw=c(10, 11, 12)),
@@ -54,6 +55,34 @@ test_that("combine_two_data_frames() works", {
     }
   )
 
+  # dates
+  expect_equal(
+    casteval:::combine_two_data_frames(
+      data.frame(time=c(lubridate::ymd("2024-01-01"),lubridate::ymd("2024-01-02"),lubridate::ymd("2024-01-03")), raw=c(10, 11, 12)),
+      data.frame(time=c(lubridate::ymd("2024-01-03"),lubridate::ymd("2024-01-02"),lubridate::ymd("2024-01-01")), raw=c(20, 21, 22))
+    ),
+    {
+      df <- data.frame(time=c(lubridate::ymd("2024-01-01"),lubridate::ymd("2024-01-02"),lubridate::ymd("2024-01-03")))
+      df$raw <- list(c(10, 22), c(11, 21), c(12, 20))
+      df
+    }
+  )
+
+  # date-times
+  expect_equal(
+    casteval:::combine_two_data_frames(
+      data.frame(time=c(lubridate::ymd_hms("2024-01-01_01-01-01"),lubridate::ymd_hms("2024-01-02_01-01-01"),lubridate::ymd_hms("2024-01-03_01-01-01")), raw=c(10, 11, 12)),
+      data.frame(time=c(lubridate::ymd_hms("2024-01-03_01-01-01"),lubridate::ymd_hms("2024-01-02_01-01-01"),lubridate::ymd_hms("2024-01-01_01-01-01")), raw=c(20, 21, 22))
+    ),
+    {
+      df <- data.frame(time=c(lubridate::ymd_hms("2024-01-01_01-01-01"),lubridate::ymd_hms("2024-01-02_01-01-01"),lubridate::ymd_hms("2024-01-03_01-01-01")))
+      df$raw <- list(c(10, 22), c(11, 21), c(12, 20))
+      df
+    }
+  )
+})
+
+test_that("combine_two_data_frames() handles NAs correctly", {
   expect_equal(
     casteval:::combine_two_data_frames(
       data.frame(time=c(1, 2, 3), raw=c(10, 11, 12)),
@@ -66,4 +95,35 @@ test_that("combine_two_data_frames() works", {
     }
   )
 })
+
+test_that("combine_two_data_frames() works with list columns", {
+  # df1 list column
+  expect_equal(
+    casteval:::combine_two_data_frames(
+      # we use tibbles here for convenience
+      dplyr::tibble(time=c(1,2,3), raw=list(c(10,11), c(12,13), c(14,15))),
+      dplyr::tibble(time=c(2,3,4), raw=c(20,21,22))
+    ),
+    dplyr::tibble(time=c(1,2,3,4), raw=list(c(10,11,NA), c(12,13,20), c(14,15,21), c(NA,NA,22)))
+  )
+
+  # df2 list column
+  expect_equal(
+    casteval:::combine_two_data_frames(
+      dplyr::tibble(time=c(2,3,4), raw=c(20,21,22)),
+      dplyr::tibble(time=c(1,2,3), raw=list(c(10,11), c(12,13), c(14,15)))
+    ),
+    dplyr::tibble(time=c(2,3,4,1), raw=list(c(20,12,13), c(21,14,15), c(22,NA,NA), c(NA,10,11)))
+  )
+
+  # both list column
+  expect_equal(
+    casteval:::combine_two_data_frames(
+      dplyr::tibble(time=c(1,2,3), raw=list(10, 11, 12)),
+      dplyr::tibble(time=c(2,3,4), raw=list(20, 21, 22))
+    ),
+    dplyr::tibble(time=c(1,2,3,4), raw=list(c(10,NA), c(11,20), c(12,21), c(NA,22)))
+  )
+})
+
 
