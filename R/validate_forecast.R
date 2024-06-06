6d1eab9a684c934d3e0518c5f575891b21531f77
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-06 14:01:24 -0400

    validate_forecast()

---
+++ R/validate_forecast.R
@@ -0,0 +1,55 @@
+#' Validate forecast in named-list format
+#'
+#' `validate_forecast()` validates a forecast by checking that it:
+#' - is a named list
+#' - contains a `time_type` field
+#' - contains a `data_types` field
+#' - contains a valid data frame
+#' - data frame format matches `time_type` and `data_type`
+#' If any of these conditions fail, it stops with an appropriate an error message
+#'
+#' @param fcst The object to be validated
+#'
+#' @returns NULL if valid (error if invalid)
+#'
+#' @examples
+#' #TBD
+validate_forecast <- function(fcst) {
+    # must be list
+    if(!is.list(fcst)) {
+        stop("forecast must be named list")
+    }
+
+    # data frames pass is.list() but are probably a sign of a mistake
+    if(is.data.frame(fcst)) {
+        stop("forecast must be named list containing data, not just a data frame")
+    }
+
+    # `time_type` present
+    if(! "time_type" %in% names(fcst)) {
+        stop("forecast must specify `time_type`")
+    }
+
+    # `data_types` present
+    if(! "data_types" %in% names(fcst)) {
+        stop("forecast must specify `data_types`")
+    }
+
+    # `data` present
+    if(! "data" %in% names(fcst)) {
+        stop("forecast must contain `data`")
+    }
+
+    # get_format() both validates the data frame and returns its format to us
+    fmt <- get_format(fcst$data)
+    
+    # check that the formats are consistent
+    if(fcst$time_type != fmt$time_type) {
+        stop("stated time type does not match data frame time type")
+    }
+    if(!identical(sort(fcst$data_types), sort(fmt$data_types))) {
+        stop("stated data types do not match data frame data types")
+    }
+    
+    NULL
+}
