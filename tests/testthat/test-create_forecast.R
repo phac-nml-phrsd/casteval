2d3cfd4be47dcd93b0e55b640daaed1deed39b76
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-07 14:35:43 -0400

    basic input validation tests

--- tests/testthat/test-create_forecast.R
+++ tests/testthat/test-create_forecast.R
@@ -1,3 +1,6 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("create_forecast() does input validation", {
+  expect_error(create_forecast(5), "has invalid type")
+  expect_error(create_forecast(list()), "list of data frames is empty")
+  expect_error(create_forecast(list(5, data.frame())), "list.*non-data-frames")
+  
 })
