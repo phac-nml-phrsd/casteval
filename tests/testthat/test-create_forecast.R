71df9bfb838b0132226b8b222d4c8329bba5fa4a
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-07 14:44:06 -0400

    create_forecast() list consistency tests

--- tests/testthat/test-create_forecast.R
+++ tests/testthat/test-create_forecast.R
@@ -3,4 +3,20 @@ test_that("create_forecast() does input validation", {
   expect_error(create_forecast(list()), "list of data frames is empty")
   expect_error(create_forecast(list(5, data.frame())), "list.*non-data-frames")
   
+  expect_error(
+    create_forecast(list(
+      data.frame(time=1,raw=2),
+      data.frame(time=lubridate:::ymd("2024-01-01"),raw=2),
+      data.frame(time=3,raw=4)
+    )),
+    "all data frames must have same time type"
+  )
+
+  expect_error(
+    create_forecast(list(
+      data.frame(time=1,raw=2),
+      data.frame(time=2,mean=5)
+    )),
+    "all data frames must contain raw data"
+  )
 })
