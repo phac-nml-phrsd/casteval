128a59c70bfea313eb5d7732baace17649ccde63
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-14 12:02:42 -0400

    neglog() work

--- R/neglog.R
+++ R/neglog.R
@@ -16,7 +16,28 @@
 #' @examples
 #' 
 neglog <- function(fcst, obs) {
+    # validate & filter
+    validate_fcst_obs_pair(fcst, obs)
+    df <- filter_forecast_time(fcst$data, fcst$forecast_time)
 
+    # demand raw values
+    if(! "raw" %in% fcst$data_types) {
+        stop("raw data needed to calculate log score")
+    }
+
+    # remove NAs & prepare for join
+    df$raw <- purrr::map(df$raw, ~ .x[!is.na(.x)]) |> dplyr::select(time, raw) |>
+      # remove rows with no data
+      dplyr::filter(as.logical(purrr::map(raw, ~ length(.x) != 0)))
+    obs <- dplyr::filter(obs, !is.na(raw)) |> dplyr::rename(obs=raw)
+
+    # join & check for overlap
+    df <- dplyr::inner_join(df, obs, dplyr::join_by(time))
+    if(nrow(df) == 0) {
+        stop("observations don't overlap with forecast data at all")
+    }
+
+    df$score <- purrr::map2(df$raw, df$obs)
 }
 
 #' Get negative-log-frequency score for single time point
