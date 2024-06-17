2c349df847b6d0c61164682ec06258c3f7c6510d
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-17 12:03:27 -0400

    work on neglog() and tests

--- R/neglog.R
+++ R/neglog.R
@@ -7,6 +7,12 @@
 #'
 #' @param fcst The forecast (see `create_forecast()` output).
 #' @param obs The observations data frame.
+#' @param at A time (compatible with `fcst` and `obs`).
+#'  If specified, the score for this time point will be returned.
+#'  Mutually exclusive with `after`.
+#' @param after An integer. If specified, the score at
+#'  time `fcst$forecast_time + after` will be returned.
+#'  Mutually exclusive with `at`.
 #'
 #' @returns A data frame containing times, raw data, observations, and scores for those times.
 #' @export
@@ -15,25 +21,22 @@
 #' @examples
 #' #TODO
 neglog <- function(fcst, obs) {
+    # TODO figure parameters to specify a single time point to return the score for.
+
     # validate & filter
     validate_fcst_obs_pair(fcst, obs)
     df <- filter_forecast_time(fcst$data, fcst$forecast_time)
 
     df <- remove_raw_NAs(df)
-
-    # prepare for join
-    df |> 
-      dplyr::select(time, raw) |>
-      # remove rows with no data
-      dplyr::filter(as.logical(purrr::map(raw, ~ length(.x) >= 2)))
-    obs <- dplyr::filter(obs, !is.na(raw)) |> dplyr::rename(obs=raw)
-
-    # join & check for overlap
-    df <- dplyr::inner_join(df, obs, dplyr::join_by(time))
-    if(nrow(df) == 0) {
-        stop("observations don't overlap with forecast data at all")
+    # KDE requires at least 2 data points, so check for that after removing NAs
+    if(any(as.logical(purrr::map(df$raw, ~ length(.x) < 2)))) {
+        stop("at least 2 raw data points for each time point required to calculate KDE")
     }
 
+    # join
+    df <- df |> dplyr::select(time, raw) |> join_fcst_obs(obs)
+
+    # score using KDE
     df$score <- purrr::map2(df$obs, df$raw, scoringRules::logs_sample)
     
     df
--- tests/testthat/test-neglog.R
+++ tests/testthat/test-neglog.R
@@ -18,19 +18,30 @@
 #   )
 # })
 
-# test_that("neglog() validates", {
-#   expect_error(
-#     neglog(
-#       create_forecast(dplyr::tibble(time=1:3, mean=4:6)),
-#       data.frame(time=1:3, raw=4:6)
-#     ),
-#     "raw data needed to calculate log score"
-#   )
+test_that("neglog() validates", {
+  expect_error(
+    neglog(
+      create_forecast(dplyr::tibble(time=1:3, mean=4:6)),
+      data.frame(time=1:3, raw=4:6)
+    ),
+    "data frame does not contain `raw` column"
+  )
 
-#   expect_error(
-#     neglog(
-#       create_forecast(dplyr::tibble(time=1:3, raw=4:6)),
+  expect_error(
+    neglog(
+      create_forecast(dplyr::tibble(time=1:2, raw=list(c(NA,2,3), c(NA,NA,4)))),
+      data.frame(time=1:2, raw=3:4)
+    ),
+    "at least 2 raw data points.*required to calculate KDE"
+  )
+})
 
-#     )
-#   )
-# })
+test_that("neglog() works", {
+  expect_equal(
+    neglog(
+      create_forecast(dplyr::tibble(time=1, raw=list(1:5))),
+      data.frame(time=1, raw=4.5)
+    ),
+    1.812532
+  )
+})
