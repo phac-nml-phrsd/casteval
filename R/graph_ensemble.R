67f48ae0324ca1874b6dcb4c32c6f63eb1b82b4a
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-19 15:35:27 -0400

    wide2long()

--- R/graph_ensemble.R
+++ R/graph_ensemble.R
@@ -17,5 +17,40 @@ graph_ensemble <- function(fcst) {
         stop("raw data needed to graph ensemble")
     }
 
-    ggplot2::ggplot()
+    num_realizations <- length(fcst$data$raw)
+}
+
+# TODO move to another file
+#' Convert raw forecast data to long format
+#'
+#' Given a data frame with raw data, convert it to long format.
+#'  Raw forecast data is usually stored in wide format, where the `raw` column
+#'  contains vectors of realization numbers.
+#'  In long format, each row contains a single data point for a single realization,
+#'  and a new column keeps track of the realization number for that row.
+#'
+#' @param df The data frame. It should contain raw forecast data (see `create_forecast()`).
+#'
+#' @returns The data frame in long format.
+#' @export
+#' @autoglobal
+#'
+#' @examples
+#' # TODO
+wide2long(df) {
+    # go through every row of the data frame
+    1:nrow(df) |>
+        purrr::map(\(row) {
+            # expand that row into its own data frame
+            dplyr::tibble(
+                # recycle the time
+                time=df[[row,"time"]],
+                # number the realizations
+                realization=1:length(df[[row,"raw"]]),
+                # and put the raw values in long form
+                raw=df[[row,"raw"]]
+            )
+        }) |>
+        # now combine them back into one data frame
+        purrr::reduce(dplyr::bind_rows)
 }
