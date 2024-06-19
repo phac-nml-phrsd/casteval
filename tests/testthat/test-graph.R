d03ea3631ad71ae7a1132fea173321348101b2c0
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-19 15:50:35 -0400

    wide2long() tests

---
+++ tests/testthat/test-graph.R
@@ -0,0 +1,20 @@
+test_that("wide2long() works", {
+  df <- dplyr::tibble(
+    time=1:3,
+    raw=list(4:6, 7:9, 10:12)
+  )
+
+  expect_equal(
+    wide2long(df),
+    dplyr::tibble(
+      time=c(1,1,1,2,2,2,3,3,3),
+      realization=c(1,2,3,1,2,3,1,2,3),
+      raw=c(4,5,6,7,8,9,10,11,12)
+    )  
+  )
+
+  expect_equal(
+    wide2long(data.frame(time=1:3, raw=4:6)),
+    dplyr::tibble(time=1:3, realization=c(1,1,1), raw=4:6)
+  )
+})
