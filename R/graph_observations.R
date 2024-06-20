2638f4b6d68c96b2eb5f2f198e5fd7b7ae0f475e
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-20 16:02:06 -0400

    finish graph_observations()

--- R/graph_observations.R
+++ R/graph_observations.R
@@ -4,19 +4,27 @@
 #'  the points can be color-coded to convey information about score, accuracy, etc.
 #'
 #' @template graph
-#' @param obs An observations data frame.
-#' @param scores A data frame containing a `time` column and a `score` column.
+#' @param obs An observations data frame. If it contains a `score` column,
+#'  the observation points will be color-coded according to the scores.
 #'
 #' @returns A ggplot object.
 #' @autoglobal
 #'
 #' @examples
 #' #TODO
-graph_observations <- function(graph=NULL, obs, scores=NULL) {
+graph_observations <- function(graph=NULL, obs) {
     if(is.null(graph)) {
         graph <- ggplot2::ggplot()
     }
 
-    #if()
+    # TODO rename observations `raw` column to `obs` everywhere, and get rid of this hack
+    if((!"obs" %in% colnames(obs)) && "raw" %in% colnames(obs)) {
+        obs <- obs |> dplyr::rename(obs=raw)
+    }
 
+    if("score" %in% colnames(obs)) {return(
+        graph + ggplot2::geom_point(ggplot2::aes(x=time, y=obs, color=score), obs)
+    )} else {return(
+        graph + ggplot2::geom_point(ggplot2::aes(x=time, y=obs), obs)
+    )}
 }
