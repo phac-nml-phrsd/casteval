47de636144a94de2417ad1f18f71cdb2287282fc
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-20 13:18:32 -0400

    fill out graph_observations() roxygen header

--- R/graph_observations.R
+++ R/graph_observations.R
@@ -3,15 +3,20 @@
 #' Graph observation points. If provided with a forecast,
 #'  the points can be color-coded to convey information about score, accuracy, etc.
 #'
+#' @template graph
+#' @param obs An observations data frame.
+#' @param fcst A forecast (see output of `create_forecast()`), or NULL.
 #'
-#' @param name desc
-#'
-#' @returns desc
-#' @export
+#' @returns A ggplot object.
 #' @autoglobal
 #'
 #' @examples
 #' #TODO
 graph_observations <- function(graph=NULL, obs, fcst=NULL) {
+    if(is.null(graph)) {
+        graph <- ggplot2::ggplot()
+    }
+    if(!is.null(fcst)) {
 
+    }
 }
