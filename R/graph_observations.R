9d7706918e4e1d23591ce553bc1a2108b9332e6e
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-20 13:01:35 -0400

    graph_observations()

---
+++ R/graph_observations.R
@@ -0,0 +1,17 @@
+#' Intelligently graph observations
+#'
+#' Graph observation points. If provided with a forecast,
+#'  the points can be color-coded to convey information about score, accuracy, etc.
+#'
+#'
+#' @param name desc
+#'
+#' @returns desc
+#' @export
+#' @autoglobal
+#'
+#' @examples
+#' #TODO
+graph_observations <- function(graph=NULL, obs, fcst=NULL) {
+
+}
