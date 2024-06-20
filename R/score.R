e5b7aadcae9388ff262f011085354e16a14e8f62
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-20 15:37:42 -0400

    add todo

--- R/score.R
+++ R/score.R
@@ -1,6 +1,8 @@
 # generic score-calculating helpers and wrappers
 # the actual scoring functions can be found in `accuracy.R`, `neglog.R`, etc.
 
+# TODO score() functional (accepts 1 or more forecasts and a scoring function)
+
 # TODO for all additional required arguments in scoring functions, set sane defaults and give a warning if the defaults are used.
 # TODO move forecast_time filtering out of the scoring functions and into a wrapper?
 
