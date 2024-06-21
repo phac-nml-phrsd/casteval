1ceaef954065a596134b8bc2f1190a5702d2756d
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-21 11:33:04 -0400

    make observations use `obs` column instead of `raw`

--- tests/testthat/test-get_format.R
+++ tests/testthat/test-get_format.R
@@ -133,12 +133,12 @@ test_that("get_format() returns correct format", {
 })
 
 test_that("get_obs_format() validates", {
-  expect_error(get_obs_format(data.frame(time=1:3, mean=4:6)), "observations require.*raw.*column")
-  expect_error(get_obs_format(dplyr::tibble(time=1:3, raw=list(1:2, 3:4, 5:6))), "observations require single-point raw data")
+  expect_error(get_obs_format(data.frame(time=1:3, raw=4:6)), "column `obs` not in data frame")
+  expect_error(get_obs_format(dplyr::tibble(time=1:3, obs=list(1:2, 3:4, 5:6))), "`obs` column must be numeric")
 })
 
 test_that("get_obs_format() works", {
-  expect_equal(get_obs_format(data.frame(time=1:3, raw=4:6)), "numeric")
-  expect_equal(get_obs_format(data.frame(time=lubridate::ymd("2024-01-01"), raw=10)), "date")
-  expect_equal(get_obs_format(data.frame(time=lubridate::ymd_hms("2024-01-01_01:02:03"), raw=10)), "date-time")
+  expect_equal(get_obs_format(data.frame(time=1:3, obs=4:6)), "numeric")
+  expect_equal(get_obs_format(data.frame(time=lubridate::ymd("2024-01-01"), obs=10)), "date")
+  expect_equal(get_obs_format(data.frame(time=lubridate::ymd_hms("2024-01-01_01:02:03"), obs=10)), "date-time")
 })
