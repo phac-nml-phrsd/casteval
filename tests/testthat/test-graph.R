425904314e3c9d67634aea2e640b8d52cca52ac0
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-26 11:28:39 -0400

    multiple legends test

--- tests/testthat/test-graph.R
+++ tests/testthat/test-graph.R
@@ -186,3 +186,13 @@ test_that("get_confidence_intervals()", {
     NULL |> graph_ensemble(fc2) |> graph_confidence_intervals(fc2, c(90,50))
   )
 })
+
+test_that("legends work", {
+  fc <- create_forecast(dplyr::tibble(
+    time=1:3,
+    raw=list(c(3,5,6,7,3), c(6,8,7,8,7), c(11,15,13,14,17))
+  ))
+  vdiffr::expect_doppelganger("legend1"
+    NULL |> graph_quantiles(fc, c(25,75)) |> graph_confidence_intervals(fc, c(50,90))
+  )
+})
