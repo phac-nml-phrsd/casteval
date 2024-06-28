f1ffff6b2033424caf3ff8accee9aa3dafbb4ad9
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-28 13:18:09 -0400

    export denmark2020 objects

--- data-raw/denmark2020.R
+++ data-raw/denmark2020.R
@@ -11,12 +11,15 @@ ens <- read_csv("data-raw/ensemble.csv") |>
     as.list()
 n <- length(ens[[1]])
 
-fc <- create_forecast(list(time=0:(n-1), real=ens), name="Denmark 5 May to 1 October 2020", forecast_time=0)
+denmark2020fc <- create_forecast(list(time=0:(n-1), real=ens), name="Denmark 5 May to 1 October 2020", forecast_time=0)
     
 hst <- read_csv("data-raw/historic.csv") |> select(-...1)
 
 m <- -length(hst[[1]])
 
-obs <- tibble(time=m:-1, obs=hst[[1]])
+denmark2020obs <- tibble(time=m:-1, obs=hst[[1]])
+
+usethis::use_data(denmark2020fc)
+usethis::use_data(denmark2020obs)
 
 # NULL |> graph_ensemble(fc) |> graph_observations(obs)
