91c479b50bbde42b70d4cbe7f63efd7b5af8e64f
Yu <daniel.yu@phac-aspc.gc.ca>
2024-06-28 14:25:52 -0400

    document denmark2020 data in data.R

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Denmark 2020 COVID-19 forecast data
+#'
+#' Forecast realizations from a Danish COVID-19 forecasting effort,
+#' concerning 5 May to 1 October 2020.
+#' 
+#' Formatted as a `casteval` forecast object.
+#' 
+#' Acquired from the below paper/repository.
+#' 
+#' @source <https://www.nature.com/articles/s41567-020-01121-y>
+#' @source <https://github.com/jonassjuul/curvestat> 
+"denmark2020fc"
+
+#' Denmark 2020 COVID-19 observations
+#'
+#' Daily COVID-19 hospitalizations in Denmark from the 55 days leading up to
+#' the 5 May 2020 forecast (see `?denmark2020fc`), from the same source.
+#' 
+#' Formatted as a two-column data frame.
+#'
+#' @source <https://www.nature.com/articles/s41567-020-01121-y>
+#' @source <https://github.com/jonassjuul/curvestat>
+"denmark2020obs"
