b3bd80fd1062aa97886442e05b7b703aaf9eac7b
Yu <daniel.yu@phac-aspc.gc.ca>
2024-07-03 16:01:34 -0400

    run check()

--- R/graph.R
+++ R/graph.R
@@ -1,24 +1,24 @@
 # TODO legends, axes, plot titles, scales. some graphs will have multiple legends so that has to be taken into consideration.
 
-#' Graph one or more forecasts
-#'
-#' Graph one or more forecasts, along with corresponding observations, quantiles, etc.
-#'
-#' @param ... One or more forecast objects (see output of `create_forecast()`).
-#' @param obs (Optional) An observations data frame.
-#'  If provided, they will be overlaid over the forecast(s) as points.
-#' @param raw (Optional) A boolean. Defaults to TRUE, in which case raw ensemble curves will be displayed.
-#' @param confs (Optional) A vector of numbers from 0 to 100.
-#'  The corresponding confidence intervals will be displayed in the resulting graph(s)
-#' @param score (Optional) A scoring function.
-#'  The function will be used to score the `obs` against the forecast(s).
-#'
-#' @returns desc
-#' @export
-#' @autoglobal
-#'
-#' @examples
-#' #TODO
+# ' Graph one or more forecasts
+# '
+# ' Graph one or more forecasts, along with corresponding observations, quantiles, etc.
+# '
+# ' @param ... One or more forecast objects (see output of `create_forecast()`).
+# ' @param obs (Optional) An observations data frame.
+# '  If provided, they will be overlaid over the forecast(s) as points.
+# ' @param raw (Optional) A boolean. Defaults to TRUE, in which case raw ensemble curves will be displayed.
+# ' @param confs (Optional) A vector of numbers from 0 to 100.
+# '  The corresponding confidence intervals will be displayed in the resulting graph(s)
+# ' @param score (Optional) A scoring function.
+# '  The function will be used to score the `obs` against the forecast(s).
+# '
+# ' @returns desc
+# ' @export
+# ' @autoglobal
+# '
+# ' @examples
+# ' #TODO
 # graph_forecasts <- function(..., obs=NULL, raw=TRUE, confs=NULL, score=NULL) {
 #     # TODO more rigorous error checking for this function
 #     # in case fcsts is a single forecast, wrap it in a list for consistency
--- tests/testthat/test-graph.R
+++ tests/testthat/test-graph.R
@@ -8,15 +8,15 @@ test_that("legends work", {
   )
 })
 
-test_that("graph_forecasts() works", {
-  fc1 <- create_forecast(dplyr::tibble(
-    time=1:3,
-    raw=list(c(4,5,4), c(7,6,6), c(8,7,6))
-  ))
+# test_that("graph_forecasts() works", {
+#   fc1 <- create_forecast(dplyr::tibble(
+#     time=1:3,
+#     raw=list(c(4,5,4), c(7,6,6), c(8,7,6))
+#   ))
 
-  fc2 <- create_forecast(dplyr::tibble(
-    time=2:4,
-    raw=list(c(17,18,19), c(19,17,18), c(19,20,22))
-  ))
+#   fc2 <- create_forecast(dplyr::tibble(
+#     time=2:4,
+#     raw=list(c(17,18,19), c(19,17,18), c(19,20,22))
+#   ))
 
-})
+# })
