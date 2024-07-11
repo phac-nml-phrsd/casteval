4a4b6cba73f2e7d1efd0be52144f93240da5b172
Yu <daniel.yu@phac-aspc.gc.ca>
2024-07-11 10:54:35 -0400

    code snippet in denmark2020 vignette

--- data-raw/owid.R
+++ data-raw/owid.R
@@ -6,8 +6,8 @@ devtools::load_all()
 
 owid <- read_csv("data-raw/owid-covid-data.csv")
 
-denmark2020owid <- owid |> filter(location=="Denmark") |> select(date, new_cases) |> mutate(date=as.numeric(date)-as.numeric(ymd("2020-05-01")))
+denmark2020owid <- owid |> filter(location=="Denmark") |> select(date, new_cases) |> mutate(date=as.numeric(date)-as.numeric(ymd("2020-05-01")), new_cases=new_cases %/% 100)
 
-denmark2020owid <- denmark2020owid |> rename(time=date, obs=new_cases)
+denmark2020owid <- denmark2020owid |> rename(time=date, obs=new_cases) |> filter(time < 150)
 
 usethis::use_data(denmark2020owid, overwrite=TRUE)
