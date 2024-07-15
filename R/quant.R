49c6fad646f020b1ccc94c49a420e433eaca6822
Yu <daniel.yu@phac-aspc.gc.ca>
2024-07-15 09:39:05 -0400

    update quant.R

--- R/quant.R
+++ R/quant.R
@@ -1,4 +1,4 @@
-# various functions for inspecting/accessing the "quant_*" columns in data frames
+# various functions for inspecting/accessing the "val_q*" columns in data frames
 
 #' Get quantile numbers from column names
 #'
@@ -7,7 +7,7 @@
 #'  This function does no input validation.
 #'
 #' @param df The data frame,
-#'  presumably containing columns named "quant_" followed by a number from 0 to 100.
+#'  presumably containing columns named "val_q" followed by a number from 0 to 100.
 #'
 #' @returns A numeric vector containing the quantiles, sorted in increasing order.
 #' @autoglobal
@@ -15,7 +15,7 @@
 #' @examples
 #' # returns c(2.5, 50, 97.5)
 #' casteval:::get_quant_percentages(
-#'   data.frame(time=1:3, quant_2.5=4:6, quant_50=7:9, quant_97.5=10:12)
+#'   data.frame(time=1:3, val_q2.5=4:6, val_q50=7:9, val_q97.5=10:12)
 #' )
 get_quant_percentages <- function(df) {
     cols <- colnames(df)
@@ -33,7 +33,7 @@ get_quant_percentages <- function(df) {
 #' Given a data frame and a quantile number, returns the corresponding
 #'  column from the data frame.
 #'
-#' @param df The data frame. Should contain a "quant_*" column corresponding to `num`
+#' @param df The data frame. Should contain a "val_q*" column corresponding to `num`
 #' @param num A number between 0 and 100 inclusive.
 #'
 #' @returns The `num` quantile column in `df`.
@@ -41,7 +41,7 @@ get_quant_percentages <- function(df) {
 #'
 #' @examples
 #' # returns 4:6
-#' casteval:::get_quant_col(data.frame(time=1:3, quant_50=4:6), 50)
+#' casteval:::get_quant_col(data.frame(time=1:3, val_q50=4:6), 50)
 get_quant_col <- function(df, num) {
     as.numeric(df[[quant_name(num)]])
 }
@@ -57,73 +57,74 @@ get_quant_col <- function(df, num) {
 #' @autoglobal
 #'
 #' @examples
-#' # "quant_50"
+#' # "val_q50"
 #' casteval:::quant_name(50)
 #' 
-#' # "quant_2.5"
+#' # "val_q2.5"
 #' casteval:::quant_name(2.5)
 quant_name <- function(num) {
     paste0("val_q", as.character(num))
 }
 
 
-#' Compute quantile data from raw data
-#'
-#' Given a column of raw data, calculate a given quantile.
-#'
-#' @param raw A numeric vector, or a list of non-empty numeric vectors. The vectors should not contain NA values.
-#'  Functions that use `raw2quant()` should make sure not to provide it NA values,
-#'  for example by using `remove_raw_NAs()`.
-#' @param perc A percentage, from 0 to 100.
-#'
-#' @returns A numeric vector with the same length as `raw`.
-#' @autoglobal
-#'
-#' @examples
-#' # c(2, 2, 4.5, 9)
-#' casteval:::raw2quant(list(1:3, 2, 3:6, 7:11), 50)
-#' # 26
-#' casteval:::raw2quant(list(0:100), 26)
-raw2quant <- function(raw, perc) {
-    if(anyNA(raw, recursive=TRUE)) {
-        stop("raw data contains NA values")
-    }
-    raw |> purrr::map(\(x) stats::quantile(x, perc/100)[[1]]) |> as.numeric()
-}
+#TODO
+# #' Compute quantile data from raw data
+# #'
+# #' Given a column of raw data, calculate a given quantile.
+# #'
+# #' @param raw A numeric vector, or a list of non-empty numeric vectors. The vectors should not contain NA values.
+# #'  Functions that use `raw2quant()` should make sure not to provide it NA values,
+# #'  for example by using `remove_raw_NAs()`.
+# #' @param perc A percentage, from 0 to 100.
+# #'
+# #' @returns A numeric vector with the same length as `raw`.
+# #' @autoglobal
+# #'
+# #' @examples
+# #' # c(2, 2, 4.5, 9)
+# #' casteval:::raw2quant(list(1:3, 2, 3:6, 7:11), 50)
+# #' # 26
+# #' casteval:::raw2quant(list(0:100), 26)
+# raw2quant <- function(raw, perc) {
+#     if(anyNA(raw, recursive=TRUE)) {
+#         stop("raw data contains NA values")
+#     }
+#     raw |> purrr::map(\(x) stats::quantile(x, perc/100)[[1]]) |> as.numeric()
+# }
 
+#TODO
+# #' Obtain quantiles from data frame
+# #'
+# #' Given a forecast data frame, compute quantiles from raw data or existing quantile columns,
+# #'  depending on what is present.
+# #'
+# #' @param df A forecast data frame. It should contain raw and/or quantile data.
+# #' @param perc A percentage, from 0 to 100.
+# #'
+# #' @returns A numeric vector with one entry for each row in `df`.
+# #' @autoglobal
+# #'
+# #' @examples
+# #' df1 <- dplyr::tibble(time=1:3, quant_50=c(1000,2000,3000), raw=list(4:6, 7:9, 10:12))
+# #' df2 <- dplyr::select(df1, time, quant_50)
+# #' 
+# #' # c(5, 8, 11)
+# #' casteval:::get_quantile(df1, 50)
+# #' 
+# #' # c(1000, 3000, 3000)
+# #' casteval:::get_quantile(df2, 50)
+# get_quantile <- function(df, perc) {
+#     # if raw present, compute quantile from raw, regardless of whether quantile columns present
+#     if("raw" %in% colnames(df)) {
+#         df <- remove_raw_NAs(df)
+#         return(raw2quant(df$raw, perc))
+#     }
 
-#' Obtain quantiles from data frame
-#'
-#' Given a forecast data frame, compute quantiles from raw data or existing quantile columns,
-#'  depending on what is present.
-#'
-#' @param df A forecast data frame. It should contain raw and/or quantile data.
-#' @param perc A percentage, from 0 to 100.
-#'
-#' @returns A numeric vector with one entry for each row in `df`.
-#' @autoglobal
-#'
-#' @examples
-#' df1 <- dplyr::tibble(time=1:3, quant_50=c(1000,2000,3000), raw=list(4:6, 7:9, 10:12))
-#' df2 <- dplyr::select(df1, time, quant_50)
-#' 
-#' # c(5, 8, 11)
-#' casteval:::get_quantile(df1, 50)
-#' 
-#' # c(1000, 3000, 3000)
-#' casteval:::get_quantile(df2, 50)
-get_quantile <- function(df, perc) {
-    # if raw present, compute quantile from raw, regardless of whether quantile columns present
-    if("raw" %in% colnames(df)) {
-        df <- remove_raw_NAs(df)
-        return(raw2quant(df$raw, perc))
-    }
+#     else if(perc %in% get_quant_percentages(df)) {
+#         return(get_quant_col(df, perc))
+#     }
 
-    else if(perc %in% get_quant_percentages(df)) {
-        return(get_quant_col(df, perc))
-    }
-
-    else {
-        stop(paste0("could not compute/obtain ", perc, "% quantile from data frame"))
-    }
-}
+#     else {
+#         stop(paste0("could not compute/obtain ", perc, "% quantile from data frame"))
+#     }
+# }
--- tests/testthat/test-quant.R
+++ tests/testthat/test-quant.R
@@ -1,73 +1,74 @@
 test_that("get_quant_percentages() works", {
   expect_equal(
-    get_quant_percentages(data.frame(quant_0=0, quant_2.5=1, quant_50=2, quant_97.5=3, quant_100=4)),
+    get_quant_percentages(data.frame(val_q0=0, val_q2.5=1, val_q50=2, val_q97.5=3, val_q100=4)),
     c(0, 2.5, 50, 97.5, 100)
   )
 })
 
 test_that("get_quant_col() works", {
   expect_equal(
-    get_quant_col(data.frame(quant_2.5=1:10, quant_50=11:20), 2.5),
+    get_quant_col(data.frame(val_q2.5=1:10, val_q50=11:20), 2.5),
     1:10
   )
 
   expect_equal(
-    get_quant_col(data.frame(quant_2.5=1:10, quant_50=11:20), 50),
+    get_quant_col(data.frame(val_q2.5=1:10, val_q50=11:20), 50),
     11:20
   )
 
   expect_equal(
-    get_quant_col(dplyr::tibble(quant_2.5=as.list(1:10), quant_50=as.list(11:20)), 50),
+    get_quant_col(dplyr::tibble(val_q2.5=as.list(1:10), val_q50=as.list(11:20)), 50),
     11:20
   )
 })
 
 test_that("quant_name() works", {
-  expect_equal(quant_name(50), "quant_50")
-  expect_equal(quant_name(0), "quant_0")
-  expect_equal(quant_name(100), "quant_100")
-  expect_equal(quant_name(2.5), "quant_2.5")
-  expect_equal(quant_name(1.2345), "quant_1.2345")
+  expect_equal(quant_name(50), "val_q50")
+  expect_equal(quant_name(0), "val_q0")
+  expect_equal(quant_name(100), "val_q100")
+  expect_equal(quant_name(2.5), "val_q2.5")
+  expect_equal(quant_name(1.2345), "val_q1.2345")
 })
 
-test_that("raw2quant() works", {
-  expect_error(raw2quant(list(c(1,2,NA),4:6)), "raw data contains NA values")
-  expect_error(raw2quant(c(1,NA,3)), "raw data contains NA values")
-  expect_error(raw2quant(list(1, NA, 3), "raw data contains NA values"))
-  expect_equal(raw2quant(list(), 50), numeric(0))
-  expect_equal(raw2quant(list(1:3, 2, 3:6, 7:11), 50), c(2, 2, 4.5, 9))
-  expect_equal(raw2quant(list(1:3, 2, 3:6, 7:11), 0), c(1, 2, 3, 7))
-  expect_equal(raw2quant(list(1:3, 2, 3:6, 7:11), 100), c(3, 2, 6, 11))
-  expect_equal(raw2quant(list(0:100), 26), 26)
-})
+#TODO
+# test_that("raw2quant() works", {
+#   expect_error(raw2quant(list(c(1,2,NA),4:6)), "raw data contains NA values")
+#   expect_error(raw2quant(c(1,NA,3)), "raw data contains NA values")
+#   expect_error(raw2quant(list(1, NA, 3), "raw data contains NA values"))
+#   expect_equal(raw2quant(list(), 50), numeric(0))
+#   expect_equal(raw2quant(list(1:3, 2, 3:6, 7:11), 50), c(2, 2, 4.5, 9))
+#   expect_equal(raw2quant(list(1:3, 2, 3:6, 7:11), 0), c(1, 2, 3, 7))
+#   expect_equal(raw2quant(list(1:3, 2, 3:6, 7:11), 100), c(3, 2, 6, 11))
+#   expect_equal(raw2quant(list(0:100), 26), 26)
+# })
 
-test_that("get_quantile() works", {
-  # error if not found
-  expect_error(
-    get_quantile(data.frame(time=1:3, quant_51=6:8), 50),
-    "could not compute/obtain.*quantile from data frame"
-  )
+# test_that("get_quantile() works", {
+#   # error if not found
+#   expect_error(
+#     get_quantile(data.frame(time=1:3, val_q51=6:8), 50),
+#     "could not compute/obtain.*quantile from data frame"
+#   )
 
-  df <- dplyr::tibble(time=1:3, quant_50=c(1000,2000,3000), raw=list(4:6, 7:9, 10:12))
+#   df <- dplyr::tibble(time=1:3, val_q50=c(1000,2000,3000), raw=list(4:6, 7:9, 10:12))
 
-  # raw supersedes quant_*
-  expect_equal(
-    get_quantile(df, 50),
-    c(5, 8, 11)
-  )
+#   # raw supersedes val_q*
+#   expect_equal(
+#     get_quantile(df, 50),
+#     c(5, 8, 11)
+#   )
 
-  expect_equal(
-    get_quantile(df |> dplyr::select(time, quant_50), 50),
-    c(1000,2000,3000)
-  )
+#   expect_equal(
+#     get_quantile(df |> dplyr::select(time, val_q50), 50),
+#     c(1000,2000,3000)
+#   )
 
-  expect_equal(
-    get_quantile(dplyr::tibble(time=1:2, raw=list(c(1,2,NA), 4:6)), 50),
-    c(1.5, 5)
-  )
+#   expect_equal(
+#     get_quantile(dplyr::tibble(time=1:2, raw=list(c(1,2,NA), 4:6)), 50),
+#     c(1.5, 5)
+#   )
 
-  expect_error(
-    get_quantile(dplyr::tibble(time=1:2, raw=c(NA,2)), 50),
-    "forecast data frame contains row with no raw data"
-  )
-})
+#   expect_error(
+#     get_quantile(dplyr::tibble(time=1:2, raw=c(NA,2)), 50),
+#     "forecast data frame contains row with no raw data"
+#   )
+# })
