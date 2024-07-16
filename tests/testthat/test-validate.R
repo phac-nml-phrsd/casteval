254a63686977b20a444a2b2009aabd4e38b0e5ea
Yu <daniel.yu@phac-aspc.gc.ca>
2024-07-16 13:12:43 -0400

    reinstate validate_time() tests

--- tests/testthat/test-validate.R
+++ tests/testthat/test-validate.R
@@ -34,49 +34,49 @@ test_that("validate_forecast() works", {
   )
 })
 
-# test_that("validate_time() works", {
-#   expect_equal(
-#     validate_time(5, create_forecast(data.frame(time=6,raw=7))),
-#     NULL
-#   )
+test_that("validate_time() works", {
+  expect_equal(
+    validate_time(5, create_forecast(data.frame(time=6,val=7))),
+    NULL
+  )
 
-#   expect_equal(
-#     validate_time(lubridate::ymd("2024-01-02"), create_forecast(data.frame(time=lubridate::ymd("2024-01-01"), raw=7))),
-#     NULL
-#   )
+  expect_equal(
+    validate_time(lubridate::ymd("2024-01-02"), create_forecast(data.frame(time=lubridate::ymd("2024-01-01"), val=7))),
+    NULL
+  )
 
-#   expect_equal(
-#     validate_time(
-#       lubridate::ymd_hms("2024-01-01_03:03:03"),
-#       create_forecast(data.frame(time=lubridate::ymd_hms("2024-01-01_04:04:04"),raw=8))
-#     ),
-#     NULL
-#   )
+  expect_equal(
+    validate_time(
+      lubridate::ymd_hms("2024-01-01_03:03:03"),
+      create_forecast(data.frame(time=lubridate::ymd_hms("2024-01-01_04:04:04"),val=8))
+    ),
+    NULL
+  )
   
-#   expect_error(
-#     validate_time(
-#       lubridate::ymd_hms("2024-01-01_04:04:04"),
-#       create_forecast(data.frame(time=5,raw=6))
-#     ),
-#     "type of `t` does not match `fcst\\$time_type`"
-#   )
+  expect_error(
+    validate_time(
+      lubridate::ymd_hms("2024-01-01_04:04:04"),
+      create_forecast(data.frame(time=5,val=6))
+    ),
+    "type of `t` does not match `fcst\\$time_type`"
+  )
 
-#   expect_error(
-#     validate_time(
-#       lubridate::ymd("2024-01-01"),
-#       create_forecast(data.frame(time=lubridate::ymd_hms("2024-01-01_00:00:00"),raw=6))
-#     ),
-#     "type of `t` does not match `fcst\\$time_type`"
-#   )
+  expect_error(
+    validate_time(
+      lubridate::ymd("2024-01-01"),
+      create_forecast(data.frame(time=lubridate::ymd_hms("2024-01-01_00:00:00"),val=6))
+    ),
+    "type of `t` does not match `fcst\\$time_type`"
+  )
 
-#   expect_error(
-#     validate_time(
-#       lubridate::ymd_hms("2024-01-01_00:00:00"),
-#       create_forecast(data.frame(time=lubridate::ymd("2024-01-01"),raw=7))
-#     ),
-#     "type of `t` does not match `fcst\\$time_type`"
-#   )
-# })
+  expect_error(
+    validate_time(
+      lubridate::ymd_hms("2024-01-01_00:00:00"),
+      create_forecast(data.frame(time=lubridate::ymd("2024-01-01"),val=7))
+    ),
+    "type of `t` does not match `fcst\\$time_type`"
+  )
+})
 
 test_that("validate_obs() works", {
   expect_error(validate_obs(list(data=data.frame(time=1,val_obs=3))), "obs must be data frame")
