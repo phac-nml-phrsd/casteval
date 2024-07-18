608b7101ecff94a3d51c3e69346b35b76ffebe68
Yu <daniel.yu@phac-aspc.gc.ca>
2024-07-18 15:20:13 -0400

    remove wide2long()

--- R/graph_ensemble.R
+++ R/graph_ensemble.R
@@ -26,8 +26,8 @@ graph_ensemble <- function(graph=NULL, fcst, alpha=NULL) {
         graph <- ggplot2::ggplot()
     }
 
-    if(! "raw" %in% fcst$data_types) {
-        stop("raw data needed to graph ensemble")
+    if(! "val" %in% fcst$data_types) {
+        stop("unsummarized data needed to graph ensemble")
     }
 
     if(is.null(alpha)) {
@@ -39,53 +39,5 @@ graph_ensemble <- function(graph=NULL, fcst, alpha=NULL) {
 
     }
 
-    # convert to long format for easy ggplot interfacing
-    df <- wide2long(fcst$data)
-
-    graph + ggplot2::geom_line(ggplot2::aes(x=time, y=raw, group=realization), alpha=alpha, df)
-}
-
-
-#' Convert raw forecast data to long format
-#'
-#' Given a data frame with raw data, convert it to long format.
-#'  Raw forecast data is usually stored in wide format, where the `raw` column
-#'  contains vectors of realization numbers.
-#'  In long format, each row contains a single data point for a single realization,
-#'  and a new column keeps track of the realization number for that row.
-#'
-#' @param df The data frame. It should contain raw forecast data (see `create_forecast()`).
-#'
-#' @returns The data frame in long format.
-#' @autoglobal
-#'
-#' @examples
-#' # dplyr::tibble(
-#' #   time=c(1,1,1,2,2,2,3,3,3),
-#' #   realization=c(1,2,3,1,2,3,1,2,3),
-#' #   raw=c(4,5,6,7,8,9,10,11,12)
-#' # )
-#' casteval:::wide2long(dplyr::tibble(
-#'   time=1:3,
-#'   raw=list(4:6, 7:9, 10:12)
-#' ))
-wide2long <- function(df) {
-    # TODO make this preserve rows other than `time` and `raw`. or make it obsolete by changing the raw format
-    # go through every row of the data frame
-    1:nrow(df) |>
-        purrr::map(\(row) {
-            # expand that row into its own data frame
-            dplyr::tibble(
-                # recycle the time
-                time=df[[row,"time"]],
-                # number the realizations
-                realization=1:length(df[[row,"raw"]][[1]]),
-                # and put the raw values in long form
-                raw=df[[row,"raw"]][[1]]
-            )
-        }) |>
-        # now combine them back into one data frame
-        purrr::reduce(dplyr::bind_rows) |>
-        # remove NA rows
-        dplyr::filter(!is.na(raw))
+    graph + ggplot2::geom_line(ggplot2::aes(x=time, y=val, group=sim), alpha=alpha, df)
 }
--- tests/testthat/test-graph_ensemble.R
+++ tests/testthat/test-graph_ensemble.R
@@ -1,38 +1,3 @@
-test_that("wide2long() works", {
-  df <- dplyr::tibble(
-    time=1:3,
-    raw=list(4:6, 7:9, 10:12)
-  )
-
-  expect_equal(
-    wide2long(df),
-    dplyr::tibble(
-      time=c(1,1,1,2,2,2,3,3,3),
-      realization=c(1,2,3,1,2,3,1,2,3),
-      raw=c(4,5,6,7,8,9,10,11,12)
-    )  
-  )
-
-  expect_equal(
-    wide2long(data.frame(time=1:3, raw=4:6)),
-    dplyr::tibble(time=1:3, realization=c(1,1,1), raw=4:6)
-  )
-
-  df2 <- dplyr::tibble(
-    time=1:4,
-    raw=list(c(NA,10,11), c(12,NA,13), c(14,15,NA), c(NA,NA,NA))
-  )
-
-  expect_equal(
-    wide2long(df2),
-    dplyr::tibble(
-      time=c(1,1,2,2,3,3),
-      realization=c(2,3,1,3,1,2),
-      raw=c(10,11,12,13,14,15)
-    )
-  )
-})
-
 test_that("graph_ensemble() works", {
   expect_error(
     graph_ensemble(NULL, create_forecast(data.frame(time=1:3,mean=4:6))),
