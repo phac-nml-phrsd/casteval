9d8a0aa680f43a39108531d5f1243ef4c3d4a71e
Yu <daniel.yu@phac-aspc.gc.ca>
2024-07-18 11:28:57 -0400

    rewrite bias()

--- R/bias.R
+++ R/bias.R
@@ -2,8 +2,8 @@
 #'
 #' Given a forecast and a set of observations, compute the bias of the forecast's predictions.
 #' `bias()` looks for forecast data in the following order:
-#' 1. raw unsummarized data
-#' 2. mean
+#' 1. raw unsummarized data (`val`)
+#' 2. mean (`val_mean`)
 #' 3. median (`val_q50`)
 #' 
 #' It uses the first that it finds to calculate bias,
@@ -25,29 +25,24 @@ bias <- function(fcst, obs, summarize=TRUE) {
     validate_fcst_obs_pair(fcst, obs)
     df <- filter_forecast_time(fcst$data, fcst$forecast_time)
 
-    # if raw present, compute the bias using all data points
-    # TODO verify if this is sound
-    if("raw" %in% fcst$data_types) {
-        # convert to long form (has to be done before join_fcst_obs() in the current state of the package)
-        # TODO fix this once raw format changed
-        df <- df |> remove_raw_NAs() |> wide2long()
-
-    } else if("mean" %in% fcst$data_types) { # next look for mean data
-        df <- df |> dplyr::mutate(raw=mean)
-    } else if("quant_50" %in% colnames(fcst$data)) {
-        df <- df |> dplyr::mutate(raw=quant_50)
-    } else {
-        stop("raw data, mean, or median required to compute bias")
+    # place the data to be scored in `prediction` column
+    if("val" %in% fcst$data_types) { # try raw data
+        df <- df |> dplyr::rename(prediction=val)
+    } else if("val_mean" %in% fcst$data_types) { # then try mean data
+        df <- df |> dplyr::rename(prediction=val_mean)
+    } else if("val_q50" %in% colnames(fcst$data)) { # then try median data
+        df <- df |> dplyr::rename(prediction=val_q50)
+    } else { # then error
+        stop("unsummarized, mean, or median forecast values required to compute bias")
     }
 
-    # map overpredict to +1 and underpredict to -1 (and equality to 0)
-    # it would be more appropriate to call the column `prediction` instead of `raw`
-    # but we call it `raw` to make it compatible with join_fcst_obs()
+    # join observations
     df <- df |> join_fcst_obs(obs) |>
-        dplyr::mutate(score=sign(raw - obs))
+        dplyr::mutate(score=sign(prediction - val_obs))
 
     if(!summarize) {
-        return(df |> dplyr::select(time, obs, score))
+        df <- df |> dplyr::group_by(time) |> dplyr::sumarize(val_obs=val_obs[[1]], score=mean(score))
+        return(df)
     }
 
     mean(df$score)
