5eb2d60917c398ab68f4819cee53042a88f6ac78
Yu <daniel.yu@phac-aspc.gc.ca>
2024-07-19 13:41:16 -0400

    convert forecast formats in tests

--- tests/testthat/test-graph_observations.R
+++ tests/testthat/test-graph_observations.R
@@ -26,32 +26,40 @@ test_that("graph_observations() works", {
     NULL |> graph_quantiles(fc2) |> graph_observations(obs2)
   )
   vdiffr::expect_doppelganger("obs6",
-    NULL |> graph_quantiles(fc2) |> graph_observations(accuracy(fc2, obs2, summarize=FALSE, interval=c(25, 75)))
+    NULL |> graph_quantiles(fc2) |> graph_observations(accuracy(fc2, obs2, summarize=FALSE, quants=c(25, 75)))
   )
 
   fc3 <- create_forecast(list(
     time=1:3,
     # TODO add a format for this
-    raw=list(c(3,5,6,7,3), c(6,8,7,8,7), c(11,15,13,14,17))
+    # raw=list(c(3,5,6,7,3), c(6,8,7,8,7), c(11,15,13,14,17))
+    vals = list(c(3,6,11), c(5,8,15), c(6,7,13), c(7,8,14), c(3,7,17))
   ))
 
   obs3 <- data.frame(
     time=1:3,
-    obs=c(4,10,14)
+    val_obs=c(4,10,14)
   )
   vdiffr::expect_doppelganger("obs7",
-    NULL |> graph_confidence_intervals(fc3, c(50)) |> graph_observations(accuracy(fc3, obs3, interval=c(25,75), summarize=FALSE))
+    NULL |> graph_confidence_intervals(fc3, c(50)) |> graph_observations(accuracy(fc3, obs3, quants=c(25,75), summarize=FALSE))
   )
 
-  fc4 <- create_forecast(dplyr::tibble(
+  fc4 <- create_forecast(list(
     time=1:10,
-    raw=list(
-      c(1,2,4,5,2), c(3,5,4,6,3), c(7,6,5,8,8), c(9,8,7,6,8), c(3,5,8,7,6),
-      c(9,8,7,10,8), c(10,11,13,12,9), c(15,14,15,20,10), c(9,8,10,15,14), c(6,8,7,7,5)
+    # raw=list(
+    #   c(1,2,4,5,2), c(3,5,4,6,3), c(7,6,5,8,8), c(9,8,7,6,8), c(3,5,8,7,6),
+    #   c(9,8,7,10,8), c(10,11,13,12,9), c(15,14,15,20,10), c(9,8,10,15,14), c(6,8,7,7,5)
+    # )
+    vals=list(
+      c(1,3,7,9,3,9,10,15,9,6),
+      c(2,5,6,8,3,9,10,15,9,6),
+      c(4,4,5,7,8,7,13,15,10,7),
+      c(5,6,8,6,7,10,12,20,15,7),
+      c(2,3,8,8,6,8,9,10,14,5)
     )
   ))
 
-  obs4 <- data.frame(time=1:10, obs=1:10)
+  obs4 <- data.frame(time=1:10, val_obs=1:10)
 
   vdiffr::expect_doppelganger("obs8",
     NULL |> graph_ensemble(fc4) |> graph_observations(neglog(fc4, obs4, summarize=FALSE))
