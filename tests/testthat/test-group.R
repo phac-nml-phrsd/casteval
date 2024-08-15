42bfa6740e04d32126e54330896673a3e28152c7
Daniel Yu <daniel.yu@phac-aspc.gc.ca>
2024-08-15 09:09:11 -0400

    get_group_names() tests

---
+++ tests/testthat/test-group.R
@@ -0,0 +1,8 @@
+test_that("get_group_names() works", {
+  expect_equal(get_group_names(data.frame(time=1)), character(0))
+
+  expect_equal(
+    get_group_names(data.frame(time=1, grp_variable=2, grp_scenario=3)),
+    c("variable", "scenario")
+  )
+})
