0459a95338e745f995be28a05fa514a74ab0ef3d
Daniel Yu <daniel.yu@phac-aspc.gc.ca>
2024-08-19 11:44:59 -0400

    make @template grouping more general

--- man-roxygen/grouping.R
+++ man-roxygen/grouping.R
@@ -1,5 +1,5 @@
 #' @section Grouping:
-#' If `summarize=FALSE` is passed, the data will be grouped by time before scoring.
+#' If `summarize=FALSE` is passed, the resulting scores will be grouped by time.
 #' If group columns are present, the data will be grouped by the group columns before scoring.
 #' In either case, the return value will instead be a data frame with columns for the time, observation, score, and group columns if they exist.
 #' See `vignette("casteval")` for more details.
