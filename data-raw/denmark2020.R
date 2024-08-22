c791d316f7a9aa2234fd210214c8bcb0d7c48c66
Daniel Yu <daniel.yu@phac-aspc.gc.ca>
2024-08-22 15:17:20 -0400

    export and document denmark2020df

--- data-raw/denmark2020.R
+++ data-raw/denmark2020.R
@@ -15,6 +15,8 @@ n <- length(denmark2020ens[[1]])
 times <- seq(ymd("2020-05-05"), ymd("2020-10-01"), by="days")
 denmark2020fc <- create_forecast(list(time=times, vals=denmark2020ens), name="COVID-19 Denmark May 5 to October 1 2020", forecast_time=ymd("2020-05-05"))
 
+denmark2020df <- denmark2020fc$data
+
 hst <- read_csv("data-raw/historic.csv") |> select(-...1)
 
 m <- -length(hst[[1]])
@@ -29,6 +31,7 @@ denmark2020obs <- read_csv("data-raw/statista.csv") |>
 
 usethis::use_data(denmark2020ens, overwrite=TRUE)
 usethis::use_data(denmark2020fc, overwrite=TRUE)
+usethis::use_data(denmark2020df, overwrite=TRUE)
 usethis::use_data(denmark2020obs, overwrite=TRUE)
 
 # NULL |> plot_ensemble(fc) |> plot_observations(obs)
