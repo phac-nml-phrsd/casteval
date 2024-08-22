a78bd56342bdaeb7adf1e7d26d472ba320f4981b
Daniel Yu <daniel.yu@phac-aspc.gc.ca>
2024-08-22 16:43:39 -0400

    facets in plot_ensemble()

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -11,4 +11,23 @@ test_that("facets work", {
   vdiffr::expect_doppelganger("facet3",
     NULL |> plot_observations(groups_obs |> dplyr::filter(grp_province=="ON"))
   )
+
+  vdiffr::expect_doppelganger("facet4",
+    NULL |> plot_observations(groups_obs |> dplyr::filter(grp_province=="ON", grp_scenario==1))
+  )
+
+  expect_error(
+    NULL |> plot_observations(groups_obs),
+    "more than 2 groups contain multiple values"
+  )
+
+  vdiffr::expect_doppelganger("facet5",
+    NULL |> plot_ensemble(create_forecast(groups2 |> dplyr::filter(grp_province=="ON")))
+  )
+
+  vdiffr::expect_doppelganger("facet6",
+    NULL |> plot_ensemble(create_forecast(groups2 |> dplyr::filter(grp_province=="ON", grp_variable=="case")))
+  )
+
+
 })
